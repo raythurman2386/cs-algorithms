2eec8e7fb54a296549bf18886698143db5fc09ce
Raymond Thurman <raymondthurman5@gmail.com>
2020-07-14 20:37:20 -0400

    :fire: First pass for moving zeros, test passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,19 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
     # Your code here
+    if n <= 1:
+        return 1
 
     pass
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,27 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # See if the array has any zeros
+    # FIRST PASS SOLUTION
+    if 0 not in arr:
+        return arr
 
-    pass
+    # What is the length of the current array
+    length = len(arr)
+
+    # Make a new array with no zeros
+    new_arr = [num for num in arr if num != 0]
+
+    # If the original length is less than the new array
+    # add zeros to fill in the space
+    while length > len(new_arr):
+        new_arr.append(0)
+
+    return new_arr
 
 
 if __name__ == '__main__':
