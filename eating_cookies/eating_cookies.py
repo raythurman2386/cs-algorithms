306d9ed027a2c8a32f1454f6f31d201aaded302a
Raymond Thurman <raymondthurman5@gmail.com>
2020-07-15 22:35:55 -0400

    :fire: eating cookies tests passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,15 +4,27 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
-    # if n is less that one
-    # return a 1
-    if n <= 1:
-        return 1
+    # if there are less than 3 cookies
+    # return the base case of combos
+    # tests show 1, 1, 2 for less than three cookies
+    base = [1, 1, 2]
 
-    # Cookie monster can only eat up to 3 cookies at a time
-    pass
+    if n < 3:
+        return base[n]
+
+    # After the base case
+    # f(n) = f(n-3) + f(n-2) + f(n-1)
+
+    x1 = base[0]
+    x2 = base[1]
+    x3 = base[2]
+
+    for _ in range(3, n):
+        x3, x2, x1 = x1 + x2 + x3, x3, x2
+
+    return x1 + x2 + x3
 
 
 if __name__ == "__main__":
